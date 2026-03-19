3a87e2fc2a9dd8af7854c5179dbad4cb7ed43e78
Merot-tr <merot.music@gmail.com>
2026-03-19 18:56:53 +0300

    dosya okuma ve kelime alma özellikleri

--- metinarama.c
+++ metinarama.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+int main(){
+    FILE *dosya;
+    char metin[999];
+    
+       char kelime[20];
+    printf("kelime giriniz: ");
+    scanf("%s", kelime);
+
+
+    char dosyaadi[20];
+    printf("dosya adi giriniz: ");
+    scanf("%s", dosyaadi);
+    
+    dosya = fopen(dosyaadi, "r");
+    if (dosya == NULL)
+    {
+        printf("dosya acilamadi!\n");
+        return 1;
+    }
+ 
+
+}
+
+ 
