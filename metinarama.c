13e625866b7cc298385e5ee4941472a7f6489b33
omerfarukdmr45 <fomerfaruk177@gmail.com>
2026-03-21 12:04:46 +0300

    Kelime arama özelliği eklendi.
    Added a loop to count occurrences of a word in a string.

--- metinarama.c
+++ metinarama.c
@@ -18,7 +18,18 @@ int main(){
         printf("dosya acilamadi!\n");
         return 1;
     }
- 
+ int counter=0;
+ for(int i=0; i<999; i++){
+    if(metin[i] == kelime[0]){
+        int j=0;
+        while(metin[i+j] == kelime[j] && kelime[j] != '\0'){
+            j++;
+        }
+        if(kelime[j] == '\0'){
+            counter++;
+        }
+    }
+ }
 
 }
 
